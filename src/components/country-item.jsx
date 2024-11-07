d1b68cccd5ea3b5b6a1075d183bfe835548e1177
Guiihenriiq <henri3985@gmail.com>
2024-11-06 20:44:38 -0500

    Adicionando nome dos Países

--- src/components/country-item.jsx
+++ src/components/country-item.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 export function CountryItem({ url, name }) {
     return (
-      <li className="item-container">
-        <img src={url} className="item-image" />
-        <h2 className="item-name">{name}</h2>
-      </li>
-    );
+        <Link to={'/country/${name}'}>
+            <li className="item-container">
+                <img src={url} className="Item-image" />
+                <h2 className="item-name">{name}</h2>
+            </li>
+        </Link>
+        );    
   }
   
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HomePage } from './pages/home-pages.jsx';
+import { CountryPage } from './pages/country-page.jsx';
+const router = createBrowserRouter ([
+  { path: "/", element:<><HomePage/></>},
+  { path: "/perfil", element:<><CountryPage/></>},
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}/>
+  </StrictMode>
+);
+
+
